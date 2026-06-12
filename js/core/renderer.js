a1ee0a2306e8034b4cbac8e48927d4e52e8b9304
wac-me <mejbaum@me.com>
2026-06-12 22:49:26 +0200

    Update renderer.js

---
+++ js/core/renderer.js
@@ -0,0 +1,94 @@
+import { state } from "./state.js";
+
+let video;
+let canvas;
+let ctx;
+
+
+export function initRenderer(videoElement, canvasElement) {
+
+    video = videoElement;
+    canvas = canvasElement;
+    ctx = canvas.getContext("2d");
+
+}
+
+
+export function render(){
+
+    requestAnimationFrame(render);
+
+    if(!video.videoWidth) return;
+
+
+    state.project.filters.brightness = brightness.value;
+    state.project.filters.contrast = contrast.value;
+    state.project.filters.saturation = saturation.value;
+
+
+    video.playbackRate = Number(speed.value);
+
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+    ctx.filter =
+    `
+    brightness(${brightness.value}%)
+    contrast(${contrast.value}%)
+    saturate(${saturation.value}%)
+    `;
+
+
+    ctx.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+
+    ctx.filter="none";
+
+
+    state.project.captions.forEach(c=>{
+
+        if(
+          video.currentTime >= c.start &&
+          video.currentTime <= c.end
+        ){
+
+            ctx.font=`${c.size}px Arial`;
+
+            ctx.textAlign="center";
+
+            ctx.strokeStyle="black";
+            ctx.lineWidth=4;
+
+            ctx.fillStyle=c.color;
+
+
+            ctx.strokeText(
+                c.text,
+                c.x,
+                c.y
+            );
+
+
+            ctx.fillText(
+                c.text,
+                c.x,
+                c.y
+            );
+
+        }
+
+    });
+
+}
