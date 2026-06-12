b9a6fe3a68c59eeddd6a4f26f168fbc096e9fabe
wac-me <mejbaum@me.com>
2026-06-13 00:14:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -77,17 +77,9 @@ loadProject.onchange=e=>{
  r.readAsText(f);
 };
 
-exportBtn.onclick=()=>{
- const stream=canvas.captureStream(30);
- try{realVideo.captureStream().getAudioTracks().forEach(t=>stream.addTrack(t));}catch(e){}
- const rec=new MediaRecorder(stream);
- const chunks=[];
- rec.ondataavailable=e=>e.data.size&&chunks.push(e.data);
- rec.onstop=()=>{
-  const a=document.createElement('a');
-  a.href=URL.createObjectURL(new Blob(chunks,{type:'video/webm'}));
-  a.download='export.webm'; a.click();
- };
+exportBtn.onclick = () => {
+    exportWebM();
+};
  realVideo.currentTime=Number(trimStart.value||0);
  rec.start(); realVideo.play();
  const stopAt=Number(trimEnd.value||realVideo.duration);
