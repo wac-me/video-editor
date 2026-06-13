31c12f3046ea7ea02af16519ce999f2140a90ae6
wac-me <mejbaum@me.com>
2026-06-13 14:23:58 +0200

    Update exporter.js

--- js/core/exporter.js
+++ js/core/exporter.js
@@ -1,47 +1,121 @@
 export function exportWebM(canvas, video){
 
-    const stream = canvas.captureStream(30);
+
+    const stream =
+        canvas.captureStream(30);
+
+
 
     try {
+
         video.captureStream()
         .getAudioTracks()
-        .forEach(track => stream.addTrack(track));
-    }
-    catch(e){}
+        .forEach(track=>{
+
+            stream.addTrack(track);
+
+        });
 
 
-    const recorder = new MediaRecorder(stream);
+    } catch(e){}
+
+
+
+    const recorder =
+        new MediaRecorder(stream);
+
+
 
     const chunks = [];
 
 
-    recorder.ondataavailable = e => {
+
+    recorder.ondataavailable = e=>{
+
+
         if(e.data.size){
+
             chunks.push(e.data);
+
         }
+
     };
+
 
 
     recorder.onstop = ()=>{
 
-        const blob = new Blob(
-            chunks,
-            {type:"video/webm"}
-        );
 
-        const a=document.createElement("a");
+        const blob =
+            new Blob(
+                chunks,
+                {
+                    type:"video/webm"
+                }
+            );
 
-        a.href=URL.createObjectURL(blob);
 
-        a.download="export.webm";
+
+        const url =
+            URL.createObjectURL(blob);
+
+
+
+        const a =
+            document.createElement("a");
+
+
+
+        a.href =
+            url;
+
+
+
+        a.download =
+            "export.webm";
+
+
+
+        document.body.appendChild(a);
+
 
         a.click();
+
+
+        setTimeout(()=>{
+
+            URL.revokeObjectURL(url);
+
+            a.remove();
+
+        },1000);
+
+
 
     };
 
 
+
+    // start od początku filmu
+
+    video.currentTime = 0;
+
+
+
     recorder.start();
 
+
+
     video.play();
+
+
+
+    video.onended = ()=>{
+
+
+        recorder.stop();
+
+
+    };
 
 }
