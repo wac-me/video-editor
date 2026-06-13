032831fd5a5c832fc401ee584c07c5478d09af0c
wac-me <mejbaum@me.com>
2026-06-13 12:43:32 +0200

    Update exporter.js

--- js/core/exporter.js
+++ js/core/exporter.js
@@ -1,11 +1,47 @@
-exportBtn.onclick=()=>{
- const stream=canvas.captureStream(30);
- try{realVideo.captureStream().getAudioTracks().forEach(t=>stream.addTrack(t));}catch(e){}
- const rec=new MediaRecorder(stream);
- const chunks=[];
- rec.ondataavailable=e=>e.data.size&&chunks.push(e.data);
- rec.onstop=()=>{
-  const a=document.createElement('a');
-  a.href=URL.createObjectURL(new Blob(chunks,{type:'video/webm'}));
-  a.download='export.webm'; a.click();
- };
+export function exportWebM(canvas, video){
+
+    const stream = canvas.captureStream(30);
+
+    try {
+        video.captureStream()
+        .getAudioTracks()
+        .forEach(track => stream.addTrack(track));
+    }
+    catch(e){}
+
+
+    const recorder = new MediaRecorder(stream);
+
+    const chunks = [];
+
+
+    recorder.ondataavailable = e => {
+        if(e.data.size){
+            chunks.push(e.data);
+        }
+    };
+
+
+    recorder.onstop = ()=>{
+
+        const blob = new Blob(
+            chunks,
+            {type:"video/webm"}
+        );
+
+        const a=document.createElement("a");
+
+        a.href=URL.createObjectURL(blob);
+
+        a.download="export.webm";
+
+        a.click();
+
+    };
+
+
+    recorder.start();
+
+    video.play();
+
+}
