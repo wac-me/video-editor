4a6fb95088afaf581508b9b41f8b511b4d5810ed
wac-me <mejbaum@me.com>
2026-06-15 02:20:34 +0200

    Update ui.js

--- ui-proto/js/ui.js
+++ ui-proto/js/ui.js
@@ -1,1 +1,14 @@
-ee
+function showScreen(id){
+
+document
+.querySelectorAll(".screen")
+.forEach(
+s=>s.classList.remove("active")
+);
+
+
+document
+.getElementById(id)
+.classList.add("active");
+
+}
