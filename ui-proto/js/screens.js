7fef42bdb4648f2ab9c85ed2fd56498fecb232e0
wac-me <mejbaum@me.com>
2026-06-18 06:04:29 +0200

    Update screens.js

--- ui-proto/js/screens.js
+++ ui-proto/js/screens.js
@@ -1,1 +1,74 @@
-qq
+let currentScreen="start";
+
+
+function showScreen(id){
+
+
+    const current =
+    document.getElementById(currentScreen);
+
+
+
+    const next =
+    document.getElementById(id);
+
+
+
+    if(!next)
+    return;
+
+
+
+    if(current){
+
+        current.classList.remove("active");
+
+    }
+
+
+
+    next.classList.add("active");
+
+
+
+    currentScreen=id;
+
+
+
+    initIcons();
+
+
+}
+
+
+
+function goBack(){
+
+
+    const previous={
+
+
+        editor:"start",
+
+        cut:"editor",
+
+        text:"editor",
+
+        export:"editor"
+
+
+    };
+
+
+    if(previous[currentScreen]){
+
+
+        showScreen(
+            previous[currentScreen]
+        );
+
+
+    }
+
+
+}
