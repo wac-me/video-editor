5cd1b16f0c13c7560af41d0cde71007e4c251c70
wac-me <mejbaum@me.com>
2026-06-18 14:09:30 +0200

    Create labels.js

---
+++ ui-proto/js/labels.js
@@ -0,0 +1,77 @@
+window.APP_LABELS = {
+
+
+    upload:
+    "Upload video",
+
+
+    uploadProject:
+    "or project",
+
+
+
+    cut:
+    "CUT",
+
+
+    text:
+    "TEXT",
+
+
+    save:
+    "SAVE",
+
+
+
+    speed:
+    "Speed",
+
+
+    adjust:
+    "Adjust",
+
+
+    fx:
+    "FX",
+
+
+
+    add:
+    "Add",
+
+
+    font:
+    "Font",
+
+
+
+    export:
+    "EXPORT",
+
+
+    saveProject:
+    "Save project",
+
+
+
+    exportTitle:
+    "Export",
+
+
+    webm:
+    "WEBM",
+
+
+    mp4:
+    "MP4",
+
+
+
+    videoTimeline:
+    "VIDEO TIMELINE",
+
+
+    textTimeline:
+    "TEXT TIMELINE"
+
+};
