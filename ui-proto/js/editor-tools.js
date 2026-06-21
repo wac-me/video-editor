e9ff0aa732d600b9f8e9934b9852ede4d25c408d
wac-me <mejbaum@me.com>
2026-06-21 16:35:43 +0200

    Update editor-tools.js

--- ui-proto/js/editor-tools.js
+++ ui-proto/js/editor-tools.js
@@ -1,8 +1,98 @@
-function openEditorTool(type){
- showScreen(type);
+let activeTool = "add";
+
+
+function hideToolPanels(){
+
+    document
+    .querySelectorAll(".tool-panel")
+    .forEach(panel=>{
+
+        panel.classList.remove("active");
+
+    });
+
 }
-function confirmEditorTool(){ showScreen('editor'); }
-function cancelEditorTool(){ showScreen('editor'); }
-window.openEditorTool=openEditorTool;
-window.confirmEditorTool=confirmEditorTool;
-window.cancelEditorTool=cancelEditorTool;
+
+
+
+function openEditorTool(type){
+
+
+    hideToolPanels();
+
+
+
+    const panel =
+    document.getElementById(type+"Tools");
+
+
+
+    if(panel){
+
+        panel.classList.add("active");
+
+    }
+
+
+    activeTool = type;
+
+
+
+}
+
+
+
+function closeEditorTool(){
+
+
+    hideToolPanels();
+
+
+    const add =
+    document.getElementById("addTools");
+
+
+    if(add){
+
+        add.classList.add("active");
+
+    }
+
+
+    activeTool="add";
+
+
+}
+
+
+
+function confirmEditorTool(){
+
+    closeEditorTool();
+
+}
+
+
+
+function cancelEditorTool(){
+
+    closeEditorTool();
+
+}
+
+
+
+window.openEditorTool =
+openEditorTool;
+
+
+window.closeEditorTool =
+closeEditorTool;
+
+
+window.confirmEditorTool =
+confirmEditorTool;
+
+
+window.cancelEditorTool =
+cancelEditorTool;
