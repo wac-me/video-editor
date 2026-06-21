52492048dd92c38d992f7644e9b6fb7a85e73cd6
wac-me <mejbaum@me.com>
2026-06-21 02:31:06 +0200

    Create timeline.js

---
+++ ui-proto/js/timeline.js
@@ -0,0 +1,94 @@
+const timeline = {
+
+    duration:15,
+
+    current:0
+
+
+};
+
+
+
+function formatTime(seconds){
+
+    let min=Math.floor(seconds/60);
+
+    let sec=Math.floor(seconds%60);
+
+
+    return (
+        String(min).padStart(2,"0")
+        +
+        ":"
+        +
+        String(sec).padStart(2,"0")
+    );
+
+}
+
+
+
+
+function initTimeline(){
+
+
+    const track =
+        document.querySelector(".timeline-track");
+
+
+    const playhead =
+        document.querySelector(".playhead");
+
+
+    const time =
+        document.querySelector("#currentTime");
+
+
+
+    if(!track)
+        return;
+
+
+
+    track.addEventListener(
+        "click",
+        e=>{
+
+
+            const rect =
+                track.getBoundingClientRect();
+
+
+
+            const position =
+                e.clientX - rect.left;
+
+
+
+            const percent =
+                position / rect.width;
+
+
+
+            timeline.current =
+                timeline.duration * percent;
+
+
+
+            playhead.style.left =
+                percent*100+"%";
+
+
+
+            time.textContent =
+                formatTime(
+                    timeline.current
+                );
+
+
+
+        }
+
+    );
+
+}
